d6ab16fb68b57dc83d76f63cbee4b95320007bdd
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-04-15 18:17:44 +0200

    Footer bar large screen css is set. Improvements may be done

--- src/componants/Footer/index.jsx
+++ src/componants/Footer/index.jsx
@@ -3,15 +3,16 @@ import iconcycle from '../../assets/iconcycle.svg'
 import iconmuscle from '../../assets/iconmuscle.svg'
 import iconswimm from '../../assets/iconswimm.svg'
 import iconyoga from '../../assets/iconyoga.svg'
+import './footer.scss'
 
 function Footer() {
     return(
         <div className="footer">
-            <nav>
-                <Link to='/'><img src={iconyoga} alt='iconyoga'></img></Link>
-                <Link to='/'><img src={iconswimm} alt='iconswimm'></img></Link>
-                <Link to='/'><img src={iconcycle} alt='iconcycle'></img></Link>
-                <Link to='/'><img src={iconmuscle} alt='iconmuscle'></img></Link>
+            <nav className='footer__nav'>
+                <Link className='footer__nav__link' to='/'><img src={iconyoga} alt='iconyoga'></img></Link>
+                <Link className='footer__nav__link' to='/'><img src={iconswimm} alt='iconswimm'></img></Link>
+                <Link className='footer__nav__link' to='/'><img src={iconcycle} alt='iconcycle'></img></Link>
+                <Link className='footer__nav__link' to='/'><img src={iconmuscle} alt='iconmuscle'></img></Link>
             </nav>
             <p className="footer__text">Copyright, SportSee 2020</p>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom'
+import './index.scss'
 import UserDashboard from './pages/UserDashboard'
 import Home from './pages/Home';
 import Header from './componants/Header';
